0255b846564d7a22dc5f2b5733d6ffe801611d90
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2023-10-24 10:22:52 -0600

    added file

---
+++ install_miniconda.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Instalamos miniconda 
+# Este link puede cambiar, hay que encontrar el adecuado
+# https://docs.conda.io/projects/miniconda/en/latest/
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod 777 Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+
