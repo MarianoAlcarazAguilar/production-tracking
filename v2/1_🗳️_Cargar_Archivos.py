280be34f1e594555607523e1038e5648d023ad3c
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2023-12-13 17:37:59 -0600

    cambios

--- v2/1_🗳️_Cargar_Archivos.py
+++ v2/1_🗳️_Cargar_Archivos.py
@@ -6,6 +6,18 @@ def render_page():
     clean_data = 'data/datos_produccion.parquet'
 
     liquido_cleaner = LiquidoCleaner(catalogo_file=catalogo, clean_data_file=clean_data)
+
+    files = st.file_uploader(label='sube el archivo de líquidos', accept_multiple_files=True, type=['xlsx', 'xlsm'])
+
+    if len(files) > 0:
+        for file in files:
+            try:
+                liquido_cleaner.extract_and_save_data(file=file)
+                st.success(f'{file.name} ha sido cargado de manera exitosa')
+                # Ahora vemos si hubo datos inválidos
+                st.dataframe(liquido_cleaner.bad_data)
+            except Exception as e:
+                st.error(f'Error en el archivo {file.name}: {e}')
     
 
 if __name__ == '__main__':
